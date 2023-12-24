8b5a9decbe5f9e8e8c94d8c947ac375323b5a975
DeminDenis1 <maxwelldenis888@gmail.com>
2023-12-24 18:26:06 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.DeminDA.Sprint7.Project.V3.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tyuiu.DeminDA.Sprint7.Project.V3.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
